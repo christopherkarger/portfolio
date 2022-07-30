ef5d9d42af98acf77e38b20c17bcd69a31a581f9
Christopher <karger1986@gmail.com>
2022-07-30 09:27:08 +0200

    init tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <section className="flex flex-col w-full h-screen justify-center items-center">
+        <h1 className="text-6xl uppercase font-bold mb-4">
+          Christopher Karger
+        </h1>
+        <h2 className="text-4xl">Senior Frontend Developer</h2>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
