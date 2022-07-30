c1df32ee5a977540ac6ea16e3b538d1291389bcb
Christopher <karger1986@gmail.com>
2022-07-30 22:44:21 +0200

    dev

---
+++ src/components/home/intro.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Intro: React.FC = () => {
+  return (
+    <section className="p-4 relative flex flex-col w-full h-screen justify-center items-center">
+      <div className="flex flex-col items-center -translate-y-4">
+        <div className="rounded-full border-2 lg:border-4 border-black w-[200px] h-[200px] lg:w-[300px] lg:h-[300px]  mb-6 overflow-hidden shrink-0">
+          <img
+            className="block w-full -translate-x-6"
+            src="me.jpg"
+            alt="Christopher"
+          />
+        </div>
+        <h1 className="text-2xl lg:text-5xl uppercase font-bold lg:mb-4">
+          Christopher Karger
+        </h1>
+        <h2 className="text-md lg:text-2xl">Frontend Developer</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
+import AboutMe from "../components/home/about-me";
+import AppDevelopment from "../components/home/app-development";
+import Intro from "../components/home/intro";
+import NativeDevelopment from "../components/home/native-development";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <section className="p-4 relative flex flex-col w-full h-screen justify-center items-center">
-        <div className="rounded-full border-2 lg:border-4 border-black w-[200px] h-[200px] lg:w-[300px] lg:h-[300px]  mb-6 overflow-hidden shrink-0">
-          <img
-            className="block w-full -translate-x-4"
-            src="me.jpg"
-            alt="Christopher"
-          />
-        </div>
-        <h1 className="text-2xl lg:text-5xl uppercase font-bold lg:mb-4">
-          Christopher Karger
-        </h1>
-        <h2 className="text-md lg:text-2xl">Senior Frontend Developer</h2>
-      </section>
+      <Intro />
+      <AboutMe />
+      <AppDevelopment />
+      <NativeDevelopment />
     </div>
   );
 };
