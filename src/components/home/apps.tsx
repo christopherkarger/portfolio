19659bcbea8afe78c96d6887cb1b212ca5ad6b95
Christopher <karger1986@gmail.com>
2022-08-07 11:00:20 +0200

    added parralax

--- src/components/home/apps.tsx
+++ src/components/home/apps.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from "react";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import Section from "../section/section";
 
 const Apps: React.FC = () => {
+  const [speed, setSpeed] = useState(0);
+
+  useEffect(() => {
+    if (window && window.innerWidth > 1023) {
+      setSpeed(15);
+    }
+  }, []);
+
   return (
     <Section headline="Apps">
       <div className="flex flex-col lg:flex-row lg:justify-between lg:gap-20">
@@ -27,18 +37,24 @@ const Apps: React.FC = () => {
             as interactive summary pages and share them with the world.
           </p>
         </div>
-        <div className="lg:pb-[100px] relative z-10 flex flex-col-reverse lg:block">
-          <img
-            className="block ml-auto lg:w-[90%] "
-            src="images/app2.png"
-            alt="Christopher"
-          />
-          <img
-            className="block lg:w-[80%] lg:absolute top-[35%] mb-8 lg:mb-0"
-            src="images/app1.png"
-            alt="Christopher"
-          />
-        </div>
+        <ParallaxProvider>
+          <div className="lg:pb-[100px] relative z-10 flex flex-col-reverse lg:block">
+            <img
+              className="block ml-auto lg:w-[90%] "
+              src="images/app2.png"
+              alt="Christopher"
+            />
+            <div className="lg:absolute lg:top-[35%]">
+              <Parallax speed={speed}>
+                <img
+                  className="block lg:w-[80%] mb-8 lg:mb-0"
+                  src="images/app1.png"
+                  alt="Christopher"
+                />
+              </Parallax>
+            </div>
+          </div>
+        </ParallaxProvider>
       </div>
     </Section>
   );
