ce6ceb251cf1c3c4912d4cce60c75bced12d57db
Christopher Karger <karger1986@gmail.com>
2024-08-27 18:21:52 +0200

    syn

--- src/components/google/GoogleAnalytics.tsx
+++ src/components/google/GoogleAnalytics.tsx
@@ -5,16 +5,19 @@ const GoogleAnalytics = () => {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       />
-      <script id="" async>
-        {`
-              window.dataLayer = window.dataLayer || [];
+      <script
+        async
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
               page_path: window.location.pathname,
-              });
-          `}
-      </script>
+            });
+            `,
+        }}
+      ></script>
     </>
   );
 };
