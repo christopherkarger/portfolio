d29130a003014a2955dca8b825fb24181ceb1a1e
Christopher Karger <karger1986@gmail.com>
2024-08-29 10:30:19 +0200

    disable ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import type { AppProps } from "next/app";
-import GoogleAnalytics from "../components/google/GoogleAnalytics";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics />
+      {/* <GoogleAnalytics /> */}
     </>
   );
 }
